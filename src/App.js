e98cb64e1f20a608722e4b7cfb4750824738965c
Carla <carlacostea@yahoo.com>
2020-08-19 12:02:42 +0300

    props and state

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
         <h1>I'm a react App</h1>
         <p>This is really working</p>
-        <button onClick>Switch Name</button>
+        <button onClick={}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Code</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
