80484e8c7779792ba4f9760b0115ec396c5844da
Carla <carlacostea@yahoo.com>
2020-08-19 12:02:18 +0300

    props and state

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ class App extends Component {
       <div className="App">
         <h1>I'm a react App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
-        <Person name="Cristina" age="13" />
-        <Person name="Carla" age="30"> My hobbies: Code</Person>
-        <Person name="Max" age="28" />
+        <button onClick>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Code</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi, I\'m a React App!!!!'));
