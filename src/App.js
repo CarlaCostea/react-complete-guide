ef9cb112dae4236722793e15b44dd5772e660adb
Carla <carlacostea@yahoo.com>
2020-08-19 11:39:41 +0300

    create a functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Cristina', age: 13 },
+      { name: 'Carla', age: 30 },
+      { name: 'Max', age: 28}
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>I'm a react App</h1>
+        <p>This is really working</p>
+        <button>Switch Name</button>
+        <Person name="Cristina" age="13" />
+        <Person name="Carla" age="30"> My hobbies: Code</Person>
+        <Person name="Max" age="28" />
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi, I\'m a React App!!!!'));
   }
 }
 
