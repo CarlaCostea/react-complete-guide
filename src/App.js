a3ccf8049c811a74bf5bdf790546c157a800cdaf
Carla <carlacostea@yahoo.com>
2020-08-19 12:28:54 +0300

    manipulating the state

--- src/App.js
+++ src/App.js
@@ -8,18 +8,30 @@ class App extends Component {
     persons: [
       { name: 'Cristina', age: 13 },
       { name: 'Carla', age: 30 },
-      { name: 'Max', age: 28}
+      { name: 'Max', age: 28 }
     ]
   }
+
+  switchNameHandler = () => {
+    // DO NOT DO THIS: this.state.person[0].name = 'Costea Cristina'
+    this.setState({
+      persons: [
+        { name:'Cristina Costea', age: 13 },
+        { name: 'Carla', age: 30 },
+        { name: 'Max', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a react App</h1>
         <p>This is really working</p>
-        <button onClick={}>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: Code</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi, I\'m a React App!!!!'));
