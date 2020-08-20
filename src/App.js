f29a8751ed7c2197dc06f38d6467ec27f419ce83
Carla <carlacostea@yahoo.com>
2020-08-20 15:05:08 +0300

    add keys

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'gdf', name: 'Max', age: 28 },
+      { id: 'hd', name: 'Manu', age: 29 },
+      { id: 'dhfdf', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -70,7 +70,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
-              age={person.age} />
+              age={person.age} 
+              key={person.id}/>
           })}
         </div>
       )
