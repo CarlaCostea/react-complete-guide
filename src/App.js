2b5810228326dd7ba249344d375043dbc9c725c7
Carla <carlacostea@yahoo.com>
2020-08-20 13:34:19 +0300

    lists and state

--- src/App.js
+++ src/App.js
@@ -14,17 +14,17 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 27 }
+  //     ]
+  //   });
+  // };
 
   nameChangeHandler = (event) => {
     this.setState({
@@ -34,6 +34,12 @@ class App extends Component {
         { name: 'Stephanie', age: 27 }
       ]
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   // this syntax is recommended when we need to use THIS
@@ -58,10 +64,11 @@ class App extends Component {
       persons = (
         // convert this.state.persons to array using map Vanilla JS E6
         <div>
-          {this.state.persons.map(person => {
-            return <Person 
-            name={person.name} 
-            age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
           })}
         </div>
       )
@@ -77,6 +84,10 @@ class App extends Component {
         {persons}
       </div>
     );
+  }
+}
+
+export default App;
 
     // Render Content Conditionally
     // return (
@@ -139,10 +150,6 @@ class App extends Component {
     //   </div>
     // );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
-}
-
-export default App;
 
 // Using Hooks
 
