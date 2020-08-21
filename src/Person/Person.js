757a00b01733abafb20f22b83621ea93eddaca3b
Carla <carlacostea@yahoo.com>
2020-08-21 15:50:44 +0300

    add media queries with radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './Person.css'
 
 const person = (props) => {
+    const personStyle = {
+        // if we want to use meddia querries we need to import StyleRoot from radium and export the app wrapped in StyleRoot
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={personStyle}>
             <p onClick={props.click} >I am {props.name} and I am {props.age} years old! {props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>    
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 }
