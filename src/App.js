eac09565ba81e1beba58c077a39d4f852ce0f4eb
Carla <carlacostea@yahoo.com>
2020-08-24 11:32:12 +0300

    css modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
-//import Radium, { StyleRoot } from 'radium';
-import styled from 'styled-components';
-import './App.css';
+
+import classes from './App.css';
 import Person from './Person/Person';
-
-// inside styled.button we have to write regular css
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'darkblue' : 'lightblue'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-  background-color: ${props => props.alt ? 'darkgreen' : 'lightgreen'};
-  color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -29,18 +13,6 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   };
-
-  // switchNameHandler = (newName) => {
-  //   // console.log('Was clicked!');
-  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-  //   this.setState({
-  //     persons: [
-  //       { name: newName, age: 28 },
-  //       { name: 'Manu', age: 29 },
-  //       { name: 'Stephanie', age: 27 }
-  //     ]
-  //   });
-  // };
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex((p) => {
@@ -80,21 +52,10 @@ class App extends Component {
   }
 
   render() {
-    const buttonStyle = {
-      backgroundColor: 'lightblue',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
 
     // Handling Dynamic Content "The JS Way"
     let persons = null;
+    let btnClass = [classes.Button]
 
     if (this.state.showPersons) {
       persons = (
@@ -110,162 +71,31 @@ class App extends Component {
           })}
         </div>
       );
-
-      // buttonStyle.backgroundColor = 'darkblue'
-      // buttonStyle[':hover'] = {
-      //   backgroundColor: 'darkgreen',
-      //   color: 'black'
-      // }
+      btnClass.push(classes.Green);
     }
 
     // let classes = ['red', 'bold'].join(' '); //"red bold"
     // in order for this to work wee need to asign a string in className}
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red']
+      assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-      //<StyleRoot>
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <StyledButton
-            alt={this.state.showPersons}
-            //style={buttonStyle}
-            onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+          <p className={assignedClasses.join(' ')}>This is really working!</p>
+          <button
+            className={btnClass.join(' ')}
+            onClick={this.togglePersonsHandler}>Toggle Persons
+            </button>
           {persons}
         </div>
-      //</StyleRoot>
     );
   }
 }
 
 export default App;
-// wrapp app in Rdium
-// export default Radium(App);
-
-    // Render Content Conditionally
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //     <p>This is really working!</p>
-    //     <button
-    //       style={buttonStyle}
-    //       onClick={this.togglePersonsHandler}>Toggle Persons</button>
-    //     {
-    //       this.state.showPersons ?
-    //       // this div is converted to react create element
-    //       <div>
-    //         <Person
-    //           name={this.state.persons[0].name}
-    //           age={this.state.persons[0].age}
-    //         />
-    //         <Person
-    //           name={this.state.persons[1].name}
-    //           age={this.state.persons[1].age}
-    //           click={this.switchNameHandler.bind(this, 'Carla')}
-    //           changed={this.nameChangeHandler}
-    //         >
-    //           My Hobbies: Code
-    //     </Person>
-    //         <Person
-    //           name={this.state.persons[2].name}
-    //           age={this.state.persons[2].age}
-    //         />
-    //       </div> : null
-    //     }
-    //   </div>
-    // );
-
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //     <p>This is really working!</p>
-    //     <button
-    //       style={buttonStyle}
-    //       onClick={() => this.switchNameHandler('Max')}>Switch Name</button>
-    //     <div>
-    //       <Person
-    //         name={this.state.persons[0].name}
-    //         age={this.state.persons[0].age}
-    //       />
-    //       <Person
-    //         name={this.state.persons[1].name}
-    //         age={this.state.persons[1].age}
-    //         click={this.switchNameHandler.bind(this, 'Carla')}
-    //         changed={this.nameChangeHandler}
-    //       >
-    //         My Hobbies: Code
-    //     </Person>
-    //       <Person
-    //         name={this.state.persons[2].name}
-    //         age={this.state.persons[2].age}
-    //       />
-    //     </div>
-    //   </div>
-    // );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-
-// Using Hooks
-
-// import React, { useState } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import Person from './Person/Person';
-
-// // class App extends Component {
-// //   state = {
-// //     persons: [
-// //       { name: 'Cristina', age: 13 },
-// //       { name: 'Carla', age: 30 },
-// //       { name: 'Max', age: 28 }
-// //     ]
-// //   }
-
-// // using React Hooks - functional compoments
-// const app = props => {
-//   // use state returns an array of 2 elements. first element is current state, the second is a function wich alows us to change state
-//   // using destructuring
-//   const [personState, setPersonState] = useState({
-//     persons: [
-//       { name: 'Cristina', age: 13 },
-//       { name: 'Carla', age: 30 },
-//       { name: 'Max', age: 28 }
-//     ],
-//     // otherState: 'for testing purpose'
-//   })
-
-//   const [otherState, setOtherState] = useState('some other value')
-
-//   const switchNameHandler = () => {
-//     // DO NOT DO THIS: this.state.person[0].name = 'Costea Cristina'
-//     // setPersonState does not merge with old informations
-//     setPersonState({
-//       persons: [
-//         { name: 'Cristina Costea', age: 13 },
-//         { name: 'Carla', age: 30 },
-//         { name: 'Max', age: 27 }
-//       ],
-//       otherState: personState.otherState
-//     })
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>I'm a react App</h1>
-//       <p>This is really working</p>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-//       <Person name={personState.persons[1].name} age={personState.persons[1].age}> My hobbies: Code</Person>
-//       <Person name={personState.persons[2].name} age={personState.persons[2].age} />
-//     </div>
-//   );
-//   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi, I\'m a React App!!!!'));
-// }
-
-
-// export default app;
---
+++ src/AppCopy.js
@@ -0,0 +1,271 @@
+import React, { Component } from 'react';
+//import Radium, { StyleRoot } from 'radium';
+import styled from 'styled-components';
+import './App.css';
+import Person from './Person/Person';
+
+// inside styled.button we have to write regular css
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'darkblue' : 'lightblue'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+  background-color: ${props => props.alt ? 'darkgreen' : 'lightgreen'};
+  color: black;
+  }
+`;
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'gdf', name: 'Max', age: 28 },
+      { id: 'hd', name: 'Manu', age: 29 },
+      { id: 'dhfdf', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
+
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 27 }
+  //     ]
+  //   });
+  // };
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    // do not mutate the state directly (do not mutate the original object)
+    // const person = this.state.persons[personIndex];
+
+    // distribute all the properties of the original object to the new one
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // slice used without arguments returns a copy (a new array) of this.state.persons
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
+  // this syntax is recommended when we need to use THIS
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  }
+
+  render() {
+    const buttonStyle = {
+      backgroundColor: 'lightblue',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
+
+    // Handling Dynamic Content "The JS Way"
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        // convert this.state.persons to array using map Vanilla JS E6
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangeHandler(event, person.id)} />
+          })}
+        </div>
+      );
+
+      // buttonStyle.backgroundColor = 'darkblue'
+      // buttonStyle[':hover'] = {
+      //   backgroundColor: 'darkgreen',
+      //   color: 'black'
+      // }
+    }
+
+    // let classes = ['red', 'bold'].join(' '); //"red bold"
+    // in order for this to work wee need to asign a string in className}
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
+    return (
+      //<StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <StyledButton
+            alt={this.state.showPersons}
+            //style={buttonStyle}
+            onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+          {persons}
+        </div>
+      //</StyleRoot>
+    );
+  }
+}
+
+export default App;
+// wrapp app in Rdium
+// export default Radium(App);
+
+    // Render Content Conditionally
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //     <p>This is really working!</p>
+    //     <button
+    //       style={buttonStyle}
+    //       onClick={this.togglePersonsHandler}>Toggle Persons</button>
+    //     {
+    //       this.state.showPersons ?
+    //       // this div is converted to react create element
+    //       <div>
+    //         <Person
+    //           name={this.state.persons[0].name}
+    //           age={this.state.persons[0].age}
+    //         />
+    //         <Person
+    //           name={this.state.persons[1].name}
+    //           age={this.state.persons[1].age}
+    //           click={this.switchNameHandler.bind(this, 'Carla')}
+    //           changed={this.nameChangeHandler}
+    //         >
+    //           My Hobbies: Code
+    //     </Person>
+    //         <Person
+    //           name={this.state.persons[2].name}
+    //           age={this.state.persons[2].age}
+    //         />
+    //       </div> : null
+    //     }
+    //   </div>
+    // );
+
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //     <p>This is really working!</p>
+    //     <button
+    //       style={buttonStyle}
+    //       onClick={() => this.switchNameHandler('Max')}>Switch Name</button>
+    //     <div>
+    //       <Person
+    //         name={this.state.persons[0].name}
+    //         age={this.state.persons[0].age}
+    //       />
+    //       <Person
+    //         name={this.state.persons[1].name}
+    //         age={this.state.persons[1].age}
+    //         click={this.switchNameHandler.bind(this, 'Carla')}
+    //         changed={this.nameChangeHandler}
+    //       >
+    //         My Hobbies: Code
+    //     </Person>
+    //       <Person
+    //         name={this.state.persons[2].name}
+    //         age={this.state.persons[2].age}
+    //       />
+    //     </div>
+    //   </div>
+    // );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
+// Using Hooks
+
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Person from './Person/Person';
+
+// // class App extends Component {
+// //   state = {
+// //     persons: [
+// //       { name: 'Cristina', age: 13 },
+// //       { name: 'Carla', age: 30 },
+// //       { name: 'Max', age: 28 }
+// //     ]
+// //   }
+
+// // using React Hooks - functional compoments
+// const app = props => {
+//   // use state returns an array of 2 elements. first element is current state, the second is a function wich alows us to change state
+//   // using destructuring
+//   const [personState, setPersonState] = useState({
+//     persons: [
+//       { name: 'Cristina', age: 13 },
+//       { name: 'Carla', age: 30 },
+//       { name: 'Max', age: 28 }
+//     ],
+//     // otherState: 'for testing purpose'
+//   })
+
+//   const [otherState, setOtherState] = useState('some other value')
+
+//   const switchNameHandler = () => {
+//     // DO NOT DO THIS: this.state.person[0].name = 'Costea Cristina'
+//     // setPersonState does not merge with old informations
+//     setPersonState({
+//       persons: [
+//         { name: 'Cristina Costea', age: 13 },
+//         { name: 'Carla', age: 30 },
+//         { name: 'Max', age: 27 }
+//       ],
+//       otherState: personState.otherState
+//     })
+//   }
+
+//   return (
+//     <div className="App">
+//       <h1>I'm a react App</h1>
+//       <p>This is really working</p>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+//       <Person name={personState.persons[1].name} age={personState.persons[1].age}> My hobbies: Code</Person>
+//       <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+//     </div>
+//   );
+//   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi, I\'m a React App!!!!'));
+// }
+
+
+// export default app;
