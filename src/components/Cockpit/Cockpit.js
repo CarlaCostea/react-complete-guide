61fad705a458a1163a92356d520110d0c53596c0
Carla <carlacostea@yahoo.com>
2020-08-25 16:56:29 +0300

    refs with react hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import classes from './Cockpit.css';
 
-const cockpit = (props) => {
+const cockpit = props => {
+  const toggleBtnRef = useRef(null);
 
-    // let classes = ['red', 'bold'].join(' '); //"red bold"
-    // in order for this to work wee need to asign a string in className}
-    const assignedClasses = [];
-    let btnClass = [classes.Button];
+  //useEffect executes only after the render
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...
+    // setTimeout(() => {
+    //   alert('Saved data to cloud!');
+    // }, 1000);
+    toggleBtnRef.current.click();
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
 
-    if (props.showPersons) {
-        btnClass.push(classes.Green);
-    }
-    
-    if (props.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
 
-    return (
-        <div>
-        <h1>{props.title}</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button
-            className={btnClass.join(' ')}
-            onClick={props.clicked}>Toggle Persons
+  // let classes = ['red', 'bold'].join(' '); //"red bold"
+  // in order for this to work wee need to asign a string in className}
+  const assignedClasses = [];
+  let btnClass = [classes.Button];
+
+  if (props.showPersons) {
+    btnClass.push(classes.Green);
+  }
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button
+        ref={toggleBtnRef}
+        className={btnClass.join(' ')}
+        onClick={props.clicked}>Toggle Persons
         </button>
-        </div>
-        )
+    </div>
+  )
 }
 
-export default cockpit;
+export default React.memo(cockpit);
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,8 +7,14 @@ import withClass from '../../../hoc/withClass';
 
 // npm install --save prop-types
 class Person extends Component {
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.inputElement.focus();
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
     }
 
     render() {
@@ -20,7 +26,8 @@ class Person extends Component {
                 </p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={(inputEl) => {this.inputElement = inputEl}}
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} 
