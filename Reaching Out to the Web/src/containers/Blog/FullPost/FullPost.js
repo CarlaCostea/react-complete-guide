957536964858c5f658e48c9d36ee79cd11231505
Carla <carlacostea@yahoo.com>
2020-08-31 12:15:16 +0300

    extract route parameters

--- Reaching Out to the Web/src/containers/Blog/FullPost/FullPost.js
+++ Reaching Out to the Web/src/containers/Blog/FullPost/FullPost.js
@@ -7,10 +7,13 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
-    componentDidUpdate() {
-        if (this.props.id) {
+
+    // to add or remove components from the dom we need to use didMount instead of didUpdate
+    componentDidMount() {
+        console.log(this.props)
+        if (this.props.match.params.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get(`/posts/${this.props.id}`)
+                axios.get(`/posts/${this.props.match.params.id}`)
                     .then(response => {
                         // console.log(response);
                         this.setState({ loadedPost: response.data })
