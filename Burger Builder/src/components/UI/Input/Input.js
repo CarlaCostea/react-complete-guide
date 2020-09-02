1f41bd9b86fb08f2d613208cdc596d9c32abfb7b
Carla <carlacostea@yahoo.com>
2020-09-02 16:50:42 +0300

    show error message

--- Burger Builder/src/components/UI/Input/Input.js
+++ Burger Builder/src/components/UI/Input/Input.js
@@ -2,30 +2,30 @@ import React from 'react';
 
 import classes from './Input.css';
 
-const input = ( props ) => {
+const input = (props) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
 
-    if (props.invalid && props.shouldValidate) {
+    if (props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
     }
 
-    switch ( props.elementType ) {
-        case ( 'input' ):
+    switch (props.elementType) {
+        case ('input'):
             inputElement = <input
                 className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed} />;
             break;
-        case ( 'textarea' ):
+        case ('textarea'):
             inputElement = <textarea
                 className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed} />;
             break;
-        case ( 'select' ):
+        case ('select'):
             inputElement = (
                 <select
                     className={inputClasses.join(' ')}
@@ -47,10 +47,16 @@ const input = ( props ) => {
                 onChange={props.changed} />;
     }
 
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
+    }
+
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 
