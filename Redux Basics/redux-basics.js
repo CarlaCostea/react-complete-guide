ce2dda345ce98910e155060de6e2a7b83e34a5f4
Carla <carlacostea@yahoo.com>
2020-09-03 10:36:26 +0300

    add subscriptions

--- Redux Basics/redux-basics.js
+++ Redux Basics/redux-basics.js
@@ -27,9 +27,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Inform me when I need to getState
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
-
-// Subscription
