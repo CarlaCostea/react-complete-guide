a114101c1facef2942c60c9b71e368418e62b692
Carla <carlacostea@yahoo.com>
2020-09-03 13:26:33 +0300

    update arrays immutably

--- Redux Basics/src/store/reducer.js
+++ Redux Basics/src/store/reducer.js
@@ -19,6 +19,21 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter + action.value
             }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: action.result })
+            }
+        case 'DELETE_RESULT':
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1)
+            // update array immutably using filter (returns a new array)
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }
     }
     return state;
 };
