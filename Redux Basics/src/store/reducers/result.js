5cb59bf4122522bdf7b57fa415944ebec9120741
Carla <carlacostea@yahoo.com>
2020-09-03 14:41:04 +0300

    combine multiple reducers

--- Redux Basics/src/store/reducers/result.js
+++ Redux Basics/src/store/reducers/result.js
@@ -1,27 +1,11 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
     results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
-        case actionTypes.DECREMENT:
-            return {
-                // shorter way to update state immutably
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
         case actionTypes.STORE_RESULT:
             return {
                 ...state,
