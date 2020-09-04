49e944901fda94a7f18c969a3b87ad0e5519550f
Carla <carlacostea@yahoo.com>
2020-09-04 10:06:05 +0300

    reducer for ingredients

--- Burger Builder/src/store/reducer.js
+++ Burger Builder/src/store/reducer.js
@@ -11,7 +11,29 @@ const initialState = {
 };
 
 const reducer = ( state = initialState, action ) => {
-
+    switch ( action.type ) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                // create deep clone of object
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                },
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                },
+                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
