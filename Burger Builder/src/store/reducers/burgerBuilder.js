864b9625340e3cd7c1d0cb2b06d6712d8b1ccf78
Carla <carlacostea@yahoo.com>
2020-09-05 17:33:36 +0300

    change the order of ingredients manually

--- Burger Builder/src/store/reducers/burgerBuilder.js
+++ Burger Builder/src/store/reducers/burgerBuilder.js
@@ -13,8 +13,8 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
             return {
                 ...state,
@@ -34,18 +34,23 @@ const reducer = ( state = initialState, action ) => {
                 },
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
             };
-            case actionTypes.SET_INGREDIENTS:
-                return {
-                    ...state,
-                    ingredients: action.ingredients,
-                    // if we had an error previously now we set it to false:
-                    error: false
-                };
-                case actionTypes.FETCH_INGREDIENTS_FAILED: 
-                return {
-                    ...state,
-                    errror: true
-                }
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    bacon: action.ingredients.bacon,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat  
+                },
+                // if we had an error previously now we set it to false:
+                error: false
+            };
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return {
+                ...state,
+                errror: true
+            }
         default:
             return state;
     }
