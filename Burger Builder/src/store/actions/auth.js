7ed63aeb594247a75b88cfd51152864a454b7d16
Carla <carlacostea@yahoo.com>
2020-09-07 12:09:14 +0300

    add auth form

---
+++ Burger Builder/src/store/actions/auth.js
@@ -0,0 +1,31 @@
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+// async
+export const auth = (email, password) => {
+    return dispatch => {
+        // here we authenticate the user
+
+        // set this state
+        dispatch(authStart());
+    };
+};
